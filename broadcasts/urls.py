20c2977977aed9d266756cb5570ae09a45647cb0
Carl J. Nobile <carl.nobile@gmail.com>
2016-11-29 10:09:26 -0500

    URL patterns updates for Django 1.9.11.

--- broadcasts/urls.py
+++ broadcasts/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import get_messages, reset_messages
 
-urlpatterns = patterns(
-    '',
+
+urlpatterns = [
     url('^$', get_messages, name="broadcast_messages"),
     url('^reset/$', reset_messages, name="reset_broadcast_messages")
-)
+    ]
